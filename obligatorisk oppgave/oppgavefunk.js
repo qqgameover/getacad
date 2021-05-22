c984cfb48f2d638b0b8988c8517aac0232d3d5ba
qqgameover <kasper35@live.no>
2021-05-22 20:56:35 +0200

    Update oppgavefunk.js

--- obligatorisk oppgave/oppgavefunk.js
+++ obligatorisk oppgave/oppgavefunk.js
@@ -5,32 +5,38 @@ let loggInnKnapp = document.getElementById("login");
 let infotext = "";
 
 updateView();
-function robotTest() {}
+
+function robotcheck() {}
 
 function loggIn(username, passord) {
   let brukerNavn = "KasperL";
   let password = "GetErFlott!";
 
-  if (username === brukerNavn && passord === password) {
-    appen.innerHTML = "Velkommen til blabla! Du er nå logget inn";
+  if (!(username === brukerNavn && passord === password))
+    if (username === "" || passord === "") {
+      infotext =
+        "Du har glemt å skrive passord eller brukernavn, vennligst legg det til!";
+      updateView();
+      return "mangler noe";
+    } else if (username != brukerNavn || passord != password) {
+      infotext = "Beklager men passordet og/eller brukernavnet er feil!";
+      updateView();
+      return "Feil";
+    } else {
+      infotext = "Dette skal ikke forekomme, noe gikk galt";
+      updateView();
+      return "error";
+    }
+  else {
+    infotext = "Velkommen til blabla! Du er nå logget inn";
     passordfield.remove();
     brukernavnfield.remove();
     loggInnKnapp.remove();
     updateView();
-  } else if (username === "" || passord === "") {
-    infotext =
-      "Du har glemt å skrive passord eller brukernavn, vennligst legg det til!";
-    updateView();
-  } else if (username != brukerNavn || passord != password) {
-    infotext = "Beklager men passordet og/eller brukernavnet er feil!";
-    updateView();
-  } else {
-    infotext = "Dette skal ikke forekomme, noe gikk galt";
-    updateView();
-    return "error";
+    return "loggedin";
   }
 }
 
 function updateView() {
-  appen.innerHTML = html;
+  appen.innerHTML = infotext;
 }
