b21aa963c74da21ab85a48351a451ae1619514c6
qqgameover <kasper35@live.no>
2021-05-28 17:46:17 +0200

    Create oppgavebare.js

---
+++ oblig2/oppgavebare.js
@@ -0,0 +1,44 @@
+var numbers = [4, 3, 1, 5, 8];
+var selectedElement;
+
+function selectStolpe(index) {
+    selectedElement = index.id;
+    yesno = "enabled";
+    yesno2 = "enabled";
+    show();
+    return `Selected ${index}`
+}
+
+function fjernStolpen(index) {
+    delete numbers[(selectedElement - 1)];
+    yesno = "disabled";
+    yesno2 = "disabled";
+    selectedElement = "ingen";
+    numbers = numbers.filter(function () {
+        return true;
+    });
+    show();
+    return `removed ${index}`;
+}
+
+function editStolpen() {
+    if (!(10 < inputValue || inputValue == undefined)) {
+        numbers[selectedElement - 1] = inputValue;
+        show();
+        return `edited ${selectedElement}`;
+    } else {
+        alert("Feltet er tomt eller du har valgt et tall over 10!")
+        return "failed";
+    }
+}
+
+function addStolpe() {
+    if (!(10 < inputValue || inputValue == undefined)) {
+        numbers.push(inputValue);
+        show();
+        return "added";
+    } else {
+        alert("Feltet er tomt eller du har valgt et tall over 10!")
+        return "failed";
+    }
+}
