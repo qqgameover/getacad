9a28f0feb309fb641e6f323885d0678b8ee83f5c
qqgameover <kasper35@live.no>
2021-06-23 17:46:19 +0200

    Create Program.cs

---
+++ csopplegg/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace getacad
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int firstNum;
+			int secondNum;
+			string operation;
+			int answer;
+
+			Console.WriteLine("Enter a number");
+			firstNum = Convert.ToInt32(Console.ReadLine());
+			Convert.ToInt32(firstNum);
+
+			Console.WriteLine("Enter an operator");
+			operation = Console.ReadLine();
+
+			Console.WriteLine("Enter another number");
+			secondNum = Convert.ToInt32(Console.ReadLine());
+			Convert.ToInt32(secondNum);
+			if (operation == "/")
+			{
+				answer = firstNum / secondNum;
+				Convert.ToString(answer);
+				Console.WriteLine("Your answer is " + answer);
+			}
+			else if (operation == "+")
+			{
+				answer = firstNum + secondNum;
+				Convert.ToString(answer);
+				Console.WriteLine("Your answer is " + answer);
+			}
+			else if (operation == "-")
+			{
+				answer = firstNum - secondNum;
+				Convert.ToString(answer);
+				Console.WriteLine("Your answer is " + answer);
+			}
+			else if (operation == "*")
+			{
+				answer = firstNum * secondNum;
+				Convert.ToString(answer);
+				Console.WriteLine("Your answer is " + answer);
+			}
+		}
+	}
+}
